0be3cc1ceebe99c69fa260374249aa90d9eff639
whazys <santos_rafa1973@hotmail.com>
2021-11-28 21:18:33 -0300

    #web-financias

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,8 +1,8 @@
 import { Item } from '../types/item'
 
 export const items: Item[] = [
-    { date: new Date(2021, 11, 28), category: 'food', title: 'McDonalds', value: 32.12 },
-    { date: new Date(2021, 11, 28), category: 'food', title: 'Burguer King', value: 28 },
-    { date: new Date(2021, 11, 29), category: 'rent', title: 'Aluguel Apt', value: 2300 },
+    { date: new Date(2021, 10, 28), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2021, 10, 28), category: 'food', title: 'Burguer King', value: 28 },
+    { date: new Date(2021, 10, 29), category: 'rent', title: 'Aluguel Apt', value: 2300 },
     { date: new Date(2021, 10, 11), category: 'salary', title: 'Salário ACME', value: 4500 },
 ]
