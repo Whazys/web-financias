28db160db8d6b786bc04f8e2aa82245da05c1d67
whazys <santos_rafa1973@hotmail.com>
2021-11-29 22:04:08 -0300

    #web-financias

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { categories } from "./data/categories";
 import { items } from './data/items'
 import { getCurrentMonth, filterListByMonth } from "./helpers/dateFilter";
 import { TableArea } from './components/TableArea'
+import { InfoArea } from './components/InfoArea'
 
 const App = () => {
   const [list, setList] = useState(items)
@@ -16,6 +17,11 @@ const App = () => {
     setFilteredList( filterListByMonth(list, currentMonth))
   }, [list, currentMonth])
 
+
+  const handleMonthChange = (newMonth: string) => {
+      setCurrentMonth(newMonth)
+  }
+
   return (
       <div>
         <C.Container>
@@ -25,7 +31,10 @@ const App = () => {
 
             <C.Body>
 
-                {/* Área de informações */}
+                <InfoArea
+                    currentMonth={currentMonth}
+                    onMonthChange={handleMonthChange}
+                />
 
                 {/* Área de inserção */}
 
--- src/helpers/dateFilter.ts
+++ src/helpers/dateFilter.ts
@@ -29,11 +29,10 @@ export const formatDate = (date: Date): string => {
     return `${addZeroToDate(day)}/${addZeroToDate(month)}/${year}`;
 }
 
-const addZeroToDate = (n: number): string => {
-    if (n < 10) {
-        return `0${n}`
-    } else {
-        return `${n}`
-    }
-}
+const addZeroToDate = (n: number): string => n < 10 ? `0${n}` : `${n}`
 
+export const formatCurrentMonth = (currentMonth: string): string => {
+    let [year, month] = currentMonth.split('-')
+    let months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+    return `${months[parseInt(month) - 1]} de ${year}`
+}
